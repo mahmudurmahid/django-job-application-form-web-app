92250230daae65d432de1e71d9157c3299df4b8f
mahmudurmahid <m.mahid@icloud.com>
2025-05-05 17:24:19 +0100

    store user data into database, user can see if data has been submitted

--- job_application/views.py
+++ job_application/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
+        # Validate the form
         if form.is_valid():
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             employment_status = form.cleaned_data["employment_status"]
-            print(first_name, last_name, email, date, employment_status)
+
+            # Save the form data to the database
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date, employment_status=employment_status)
+            # Message to inform the user that the application was submitted successfully
+            messages.success(request, "Your application has been submitted successfully!")
     return render(request, "index.html")
