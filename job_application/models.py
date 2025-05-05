6bc722330373876f5cc786a602734306084675d6
mahmudurmahid <m.mahid@icloud.com>
2025-05-05 15:34:44 +0100

    form model created

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100)
+    date = models.DateField()
+    employment_status = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
