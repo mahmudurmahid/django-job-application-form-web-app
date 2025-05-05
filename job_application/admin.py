5a6bd204b45e40baa69a576b948b1d38b26b8417
mahmudurmahid <m.mahid@icloud.com>
2025-05-05 19:22:25 +0100

    admin panel created

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Form
 
 # Register your models here.
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'date', 'employment_status')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('date', 'employment_status')
+    ordering = ('first_name',)
+    readonly_fields = ('employment_status',) # Cannot change this field in the admin interface
+
+
+admin.site.register(Form, FormAdmin)
