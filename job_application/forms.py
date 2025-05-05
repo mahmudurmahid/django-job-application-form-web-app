bb48cd4a0d46b1afdb39b7b5d4512d3ed6a0bdd0
mahmudurmahid <m.mahid@icloud.com>
2025-05-05 17:16:11 +0100

    created application form class in forms, imported into views, stored user data in variables

---
+++ job_application/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class ApplicationForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField(max_length=100)
+    date = forms.DateField()
+    employment_status = forms.CharField(max_length=80)
+
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            employment_status = form.cleaned_data["employment_status"]
+            print(first_name, last_name, email, date, employment_status)
     return render(request, "index.html")
